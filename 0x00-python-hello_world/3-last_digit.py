ea0e7c4814cf6d51a0439cd0823aa4e5dc1fd851
Alef <83234291+fullstackwd@users.noreply.github.com>
2021-11-28 22:12:56 -0300

    all

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    num = ((number * -1) % 10) * -1
+else:
+    num = number % 10
+if num > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, num))
+elif num == 0:
+    print("Last digit of {} is {} and is 0".format(number, num))
+elif num < 6 and num != 0:
+    print("Last digit of {} is {} and is less\
+ than 6 and not 0".format(number, num))
+ 
