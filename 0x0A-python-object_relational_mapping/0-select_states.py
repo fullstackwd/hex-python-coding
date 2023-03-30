a8781f551c526eb8a678fc06c2fa1181c2155519
Alef <83234291+fullstackwd@users.noreply.github.com>
2023-03-30 16:27:56 -0300

    Update 0-select_states.py
    Just 0

--- 0x0A-python-object_relational_mapping/0-select_states.py
+++ 0x0A-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
 
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    """Access DB and get states"""
-    db = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states")
-    rows = c.fetchall()
+    # Check that the correct number of arguments have been passed
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
+        sys.exit(1)
+        
+    # Try to connect to the database
+    try:
+        db = MySQLdb.connect(
+            host="localhost", 
+            user=sys.argv[1], 
+            port=3306, 
+            passwd=sys.argv[2], 
+            db=sys.argv[3])
+    except MySQLdb.Error as e:
+        print(f"Error connecting to database: {e}")
+        sys.exit(1)
+    
+    # Get a cursor object and execute the query
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT * FROM states")
+    except MySQLdb.Error as e:
+        print(f"Error executing query: {e}")
+        sys.exit(1)
+    
+    # Retrieve the results and print them
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
+    
+    # Close the database connection
+    db.close()
