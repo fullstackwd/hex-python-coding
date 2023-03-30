beb16329043c4abdfc7d9a8c1cb852b2790d0fd3
Alef <83234291+fullstackwd@users.noreply.github.com>
2023-03-30 16:40:31 -0300

    Update 0-select_states.py
    Redo

--- 0x0A-python-object_relational_mapping/0-select_states.py
+++ 0x0A-python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
+"""
+connect to database
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-"""Script that lists all states from the database hbtn_0e_0_usa"""
-
-import MySQLdb
-import sys
-
-if __name__ == '__main__':
-    # Check that the correct number of arguments have been passed
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
-        sys.exit(1)
-        
-    # Try to connect to the database
-    try:
-        db = MySQLdb.connect(
-            host="localhost", 
-            user=sys.argv[1], 
-            port=3306, 
-            passwd=sys.argv[2], 
-            db=sys.argv[3])
-    except MySQLdb.Error as e:
-        print(f"Error connecting to database: {e}")
-        sys.exit(1)
-    
-    # Get a cursor object and execute the query
+    # Open database connection
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    # prepare a cursor object using cursor() method
     cursor = db.cursor()
-    try:
-        cursor.execute("SELECT * FROM states")
-    except MySQLdb.Error as e:
-        print(f"Error executing query: {e}")
-        sys.exit(1)
-    
-    # Retrieve the results and print them
-    rows = cursor.fetchall()
-    for row in rows:
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Fetch all rows using fetchall() method.
+    data = cursor.fetchall()
+    # loop through the data and print results
+    for row in data:
         print(row)
-    
-    # Close the database connection
+    # disconnect from server
     db.close()
+    
