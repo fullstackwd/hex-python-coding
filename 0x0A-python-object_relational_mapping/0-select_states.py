35dbac01073ea0fea603d418c3d384a1a3e6731a
Alef <83234291+fullstackwd@users.noreply.github.com>
2023-03-30 16:43:45 -0300

    Update 0-select_states.py
    Time

--- 0x0A-python-object_relational_mapping/0-select_states.py
+++ 0x0A-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""
-connect to database
-"""
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-    # Open database connection
-    db = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        port=3306
-    )
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
-    # execute SQL query using execute() method.
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    # Fetch all rows using fetchall() method.
-    data = cursor.fetchall()
-    # loop through the data and print results
-    for row in data:
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-    # disconnect from server
+    cur.close()
     db.close()
-    
